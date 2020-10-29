7d0d07342da894af8aa300f09a20dde726a4510b
Gary Harrower <gary@edidigital.com>
2020-10-29 00:51:36 +0000

    Init

---
+++ launch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+echo "START: Running sonar-scanner-cli on `date`"
+
+sed -i 's/use_embedded_jre=true/use_embedded_jre=false/g' $SONARQUBE_SCANNER_BIN/sonar-scanner
+
+INPUT=$(echo -n $@ | sed "s/{SONARQUBE_TOKEN}/$SONARQUBE_TOKEN/g")
+
+sonar-scanner $INPUT
+echo "END: Running sonar-scanner-cli on `date`"
